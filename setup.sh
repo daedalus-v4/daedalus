a26926edafb78dd6e59588899eb7fd655a8a0a2c
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-28 15:49:03 -0400

    update pre-commit hook

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 cat > .git/hooks/pre-commit << EOF
 #!/bin/sh
-bun x prettier --write core shared
+bun x prettier --write core shared > /dev/null
 cd dashboard
-bun x prettier --write .
+bun x prettier --write . > /dev/null
+echo "Formatted files."
 EOF
 
 chmod +x .git/hooks/pre-commit
