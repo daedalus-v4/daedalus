7abc9399766efe87775566f92c6838980cb69b19
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-28 16:28:23 -0400

    add admin commands

--- core/src/bot/modules/admin/index.ts
+++ core/src/bot/modules/admin/index.ts
@@ -13,12 +13,57 @@ export default (app: Argentium) =>
             })
             .slash((x) =>
                 x
+                    .key("admin promote")
+                    .description("promote a user to Daedalus admin")
+                    .userOption("user", "the user to promote", { required: true })
+                    .fn(defer(true))
+                    .fn(async ({ _, user }): Promise<string | BaseMessageOptions> => {
+                        if (user.id === Bun.env.OWNER) throw `${user} is the bot owner and is already an admin.`;
+
+                        const doc = await db.admins.findOneAndUpdate({ user: user.id }, { $set: { user: user.id } }, { upsert: true });
+                        if (doc) throw `${user} is already a Daedalus admin.`;
+
+                        return template.success(`${user} is now a Daedalus admin.`);
+                    }),
+            )
+            .slash((x) =>
+                x
+                    .key("admin demote")
+                    .description("demote a user from Daedalus admin")
+                    .userOption("user", "the user to demote", { required: true })
+                    .fn(defer(true))
+                    .fn(async ({ _, user }): Promise<string | BaseMessageOptions> => {
+                        if (user.id === Bun.env.OWNER) throw `${user} is the bot owner and cannot be demoted.`;
+
+                        const doc = await db.admins.findOneAndDelete({ user: user.id });
+                        if (!doc) throw `${user} is not a Daedalus admin.`;
+
+                        return template.success(`${user} is no longer a Daedalus admin.`);
+                    }),
+            )
+            .slash((x) =>
+                x
                     .key("admin eval")
                     .description("evaluate JS code")
                     .stringOption("code", "what to evaluate", { required: true })
                     .fn(defer(true))
                     .fn(async ({ _, code }): Promise<string | BaseMessageOptions> => {
-                        const result = await eval(`(async function(){${code}})()`);
+                        const result = await Object.getPrototypeOf(async function () {}).constructor(
+                            "ctx",
+                            code,
+                        )({
+                            cmd: _,
+                            channel: _.channel,
+                            guild: _.guild,
+                            me: _.guild ? await _.guild.members.fetch(_.user) : _.user,
+                            async input(full = false, others = false) {
+                                const reply = (
+                                    await _.channel!.awaitMessages(others ? { max: 1 } : { max: 1, filter: (m) => m.author.id === _.user.id })
+                                ).first()!;
+
+                                return full ? reply : reply.content;
+                            },
+                        });
 
                         if (result === undefined || result === null) return { content: `\`\`\`js\n${result}\n\`\`\`` };
 
--- core/src/lib/db.ts
+++ core/src/lib/db.ts
@@ -23,6 +23,7 @@ import {
     DbWelcomeSettings,
     DbXpSettings,
 } from "../../../shared";
+import log from "./log.js";
 
 export let _db: Db;
 export let client: MongoClient;
@@ -31,6 +32,7 @@ export async function connect() {
     client = new MongoClient(Bun.env.DB_URI!);
     await client.connect();
     _db = client.db(Bun.env.DB_NAME);
+    log.info("DB connected.");
 }
 
 class Database {
