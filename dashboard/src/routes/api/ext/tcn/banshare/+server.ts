088f7b1d9ea3bdc7363c3eb5e078c43c6d852817
hyper-neutrino <alex.yj.liao@gmail.com>
2023-11-21 02:04:37 -0500

    add TCN banshare hook

--- dashboard/src/routes/api/ext/tcn/banshare/+server.ts
+++ dashboard/src/routes/api/ext/tcn/banshare/+server.ts
@@ -1,20 +1,39 @@
 import { TCN_HMAC_KEY } from "$env/static/private";
 import crypto from "crypto";
+import { autoIncrement, db } from "shared/db.js";
 import type { RequestHandler } from "./$types.js";
 
 const key = crypto.createPrivateKey(TCN_HMAC_KEY);
 
 export const POST: RequestHandler = async ({ url }) => {
+    const server = url.searchParams.get("g");
     const userid = url.searchParams.get("u");
     const reason = url.searchParams.get("r");
     const origin = url.searchParams.get("o");
+    const tcnbot = url.searchParams.get("i");
     const hmac = url.searchParams.get("h");
 
-    if (!userid || !reason || !origin || !hmac) return new Response("", { status: 400 });
+    if (!server || !userid || !reason || !origin || !tcnbot || !hmac) return new Response("", { status: 400 });
 
-    if (!crypto.timingSafeEqual(Buffer.from(hmac, "base64url"), crypto.createHmac("sha256", key).update(`${userid} ${reason} ${origin}`).digest()))
+    if (
+        !crypto.timingSafeEqual(
+            Buffer.from(hmac, "base64url"),
+            crypto.createHmac("sha256", key).update(`${server} ${userid} ${reason} ${origin} ${tcnbot}`).digest(),
+        )
+    )
         return new Response("", { status: 403 });
 
-    // TODO
-    throw 0;
+    await db.userHistory.insertOne({
+        guild: server,
+        user: userid,
+        id: await autoIncrement(`history/${server}`),
+        type: "ban",
+        mod: tcnbot,
+        time: Date.now(),
+        duration: 0,
+        origin,
+        reason,
+    });
+
+    return new Response();
 };
