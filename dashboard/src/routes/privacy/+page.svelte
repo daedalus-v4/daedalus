a22079311833cc47754b187bacde59a64baa21da
hyper-neutrino <alex.yj.liao@gmail.com>
2023-11-28 13:54:47 -0500

    add terms and privacy pages

---
+++ dashboard/src/routes/privacy/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import A from "$lib/components/A.svelte";
+    import Container from "$lib/components/Container.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import P from "$lib/components/P.svelte";
+</script>
+
+<Header>
+    <p class="text-3xl"><b>Daedalus</b> Privacy Policy</p>
+</Header>
+
+<Container main>
+    <P>Effective as of November 29th, 2023</P>
+    <h2 class="h2">Privacy Policy</h2>
+    <P>Daedalus stores message content for the purposes of providing logs for modmail and tickets. Other messages are not stored.</P>
+    <P>
+        <ul class="list-disc pl-4">
+            <li class="pl-2">
+                If you DM the bot and confirm sending the message through modmail to a server, it will be logged and that server's staff team will be able to
+                view the contents of the thread afterwards.
+            </li>
+            <li class="pl-2">
+                If you type messages into a modmail thread from the server side, they will be stored as internal messages even if you do not send them as a
+                reply, and other staff will be able to view those messages afterwards.
+            </li>
+            <li class="pl-2">All messages within ticket channels will be stored and staff will be able to view them afterwards.</li>
+        </ul>
+    </P>
+    <P>
+        You may request redaction of specific messages or full redaction of all of your stored message content data through the
+        <A to="https://discord.gg/7TRKfSK7EU" external>support server</A>, e-mailing <A>hyperneutrino15@gmail.com</A>, or direct messaging <b>@hyperneutrino</b>
+        on Discord (you should join the support server to use it as a mutual server).
+    </P>
+    <P>
+        Otherwise, Daedalus does not store any data except what you explicitly enter into the dashboard or the bot through commands such as logging user
+        history. You may contact us through the contact methods specified in the <A to="/terms" external>Terms of Service</A> to request that your data is expunged
+        as applicable.
+    </P>
+    <P>
+        The server owner may request their server's data to be fully deleted and a user may request data that is applicable only to their own usage to be
+        removed, e.g. reminders and highlights. However, users may not request data not entered by themselves or Daedalus to be removed (e.g. user history
+        logged by servers' staff members).
+    </P>
+</Container>
---
+++ dashboard/src/routes/terms/+page.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+    import A from "$lib/components/A.svelte";
+    import Container from "$lib/components/Container.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import P from "$lib/components/P.svelte";
+</script>
+
+<Header>
+    <p class="text-3xl"><b>Daedalus</b> Terms of Service</p>
+</Header>
+
+<Container main>
+    <P>Effective as of November 29th, 2023</P>
+    <h2 class="h2">Summary</h2>
+    <P>
+        These Terms govern the use of Daedalus and are a legally binding contract between the user and servers using Daedalus ("you") and Daedalus, the Daedalus
+        development team, and all persons operating or contributing to Daedalus ("Daedalus", "we", "us").
+    </P>
+    <P>
+        This service is provided with no warranty and Daedalus is not liable to any harm resulting from uncontrollable service interruptions, unintentional
+        breaking of features (bugs, undefined behavior, etc.), or any related issues.
+    </P>
+    <P>
+        To contact the Daedalus team, the preferred method is to message <b>@hyperneutrino</b> on Discord (join the support server at
+        <A to="https://discord.gg/7TRKfSK7EU" external>https://discord.gg/7TRKfSK7EU</A> to be able to message him). Alternatively, you may email him at
+        <A>hyperneutrino15@gmail.com</A>.
+    </P>
+    <P>
+        <ul class="list-disc pl-4">
+            <li class="pl-2">
+                Usage of Daedalus is age-restricted as detailed below (users must be at least 13 years of age and minors may only use Daedalus under supervision
+                from an adult guardian, subject to laws in the user's jurisdiction).
+            </li>
+            <li class="pl-2">
+                All users, even if not European, may exercise the GDPR "right to be forgotten" and request data for their server or their user where applicable
+                to be fully expunged.
+            </li>
+            <li class="pl-2">
+                Usage of Daedalus is subject to termination at any time at the discretion of the Daedalus team as it relates to abuse in the form of deliberate
+                exploitation of bugs, flooding its ratelimits, and using Daedalus for spamming, scamming, harassment, discrimination, or other abusive
+                behaviors.
+            </li>
+        </ul>
+    </P>
+    <h2 class="h2">Age Restriction</h2>
+    <P>By using Daedalus, you confirm that you are at least 13 years of old (this is in compliance with Discord's policy and online child protection laws).</P>
+    <P>
+        If you live in a jurisdiction with laws regulating data collection and technology usage, you confirm that your usage and the collection of your data by
+        Daedalus in line with our <A to="/privacy" external>Privacy Policy</A> is legal within your jurisdiction.
+    </P>
+    <h2 class="h2">Acceptable Use</h2>
+    <P>The following, but not limited to the following, are prohibited:</P>
+    <P>
+        <ul class="list-disc pl-4">
+            <li class="pl-2">Use of Daedalus for purposes of spamming, scamming, harassment, discrimination, doxxing, or other abusive behaviors.</li>
+            <li class="pl-2">Impersonation of Daedalus or Daedalus staff for any purpose, especially to attempt to compromise other users.</li>
+            <li class="pl-2">Defamation, abuse, harassment, or threats against Daedalus or its staff.</li>
+            <li class="pl-2">
+                Promotion of illegal, dangerous, or harmful behavior using Daedalus, including but not limited to posting content forbidden by law, suicide
+                threats, posting gore or other disturbing content, encouraging use of drugs, promoting self-destructive, harmful, violent, or similar behavior,
+                etc.
+            </li>
+            <li class="pl-2">
+                Attempting to breach Daedalus through penetration testing or identifying and exploiting vulnerabilities, including the bot, the website, and all
+                related services.
+            </li>
+        </ul>
+    </P>
+    <P>Prohibited behavior is subject to termination of the right to use Daedalus at the discretion of the staff.</P>
+    <h2 class="h2">Content Rights</h2>
+    <P>
+        Except where specified otherwise, we hold and reserve all intellectual property rights for content on Daedalus and the code for the service. Users may
+        not use the content in any way that is not directly in line with proper usage of Daedalus.
+    </P>
+    <P>
+        Prohibited usage includes, but is not limited to, copying for other uses, downloading, sublicensing, transformation, publication, and creation of
+        derivative material.
+    </P>
+    <h2 class="h2">Service Interruption</h2>
+    <P>
+        Daedalus retains the right to interrupt the service for maintenance, upgrades, or other changes in relation to providing the best service possible.
+        Adequate informing of users will be done through the support server's announcement channel.
+    </P>
+    <P>
+        Within legal limits, Daedalus also retains the right to suspend or terminate the service in its entirety. In this case, Daedalus will discard all
+        personal data and information.
+    </P>
+    <P>
+        Additionally, Daedalus may become unavailable due to factors outside of Daedalus' reasonable control such as internet failures, blackouts, or other
+        issues with the infrastructure on which Discord is dependent.
+    </P>
+    <h2 class="h2">Changes to these Terms</h2>
+    <P>
+        Daedalus retains the right to modify these terms of service at any time. Users will be appropriately informed of any notable changes. Such changes only
+        apply as of the time of writing and are not retroactive.
+    </P>
+    <P>
+        Continued use of Daedalus is an implicit agreement to follow the terms of service through any revisions and changes. Users may disagree with the terms
+        of service and unbind themselves from the contract by ceasing usage of the service.
+    </P>
+    <h2 class="h2">Disputes</h2>
+    <P>
+        Users may present disputes to Daedalus through the aforementioned communication methods. While we cannot restrict users' right to legal action, we
+        humbly request you to contact us directly in case of disputes so that we may attempt to resolve them and adequately compensate you if applicable.
+    </P>
+    <h2 class="h2">Security</h2>
+    <P>
+        In the case of security-related flaws that may compromise the safety of users of the service, please report them via either contact method. Do not
+        disclose information publicly.
+    </P>
+</Container>
