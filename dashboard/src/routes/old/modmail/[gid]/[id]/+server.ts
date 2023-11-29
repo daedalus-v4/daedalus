4f16bf935c07038b780a135ddfe0ae1ef81d741b
hyper-neutrino <alex.yj.liao@gmail.com>
2023-11-29 10:54:07 -0500

    fix redirection

--- dashboard/src/routes/old/modmail/[gid]/[id]/+server.ts
+++ dashboard/src/routes/old/modmail/[gid]/[id]/+server.ts
@@ -3,7 +3,7 @@ import { db } from "shared/db.js";
 import type { RequestHandler } from "./$types.js";
 
 export const GET: RequestHandler = async ({ params: { gid, id } }) => {
-    const doc = await db.modmailThreads.findOne({ guild: gid, channel: id });
+    const doc = await db.modmailThreads.findOne({ guild: gid, user: id });
     if (!doc) throw redirect(308, "/");
 
     throw redirect(308, `/modmail/${doc.uuid}`);
