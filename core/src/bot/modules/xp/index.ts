0ccac3a5e862db66bed58963cd2913da6271a81a
hyper-neutrino <alex.yj.liao@gmail.com>
2023-11-30 00:22:59 -0500

    ???

--- core/src/bot/modules/xp/index.ts
+++ core/src/bot/modules/xp/index.ts
@@ -38,9 +38,7 @@ export default (app: Argentium) =>
                             user ??= _.user;
 
                             try {
-                                await _.editReply({
-                                    files: [{ contentType: "image/png", attachment: (await drawRankcard(_.guild!, user)) as any, name: `${user.id}-rank.png` }],
-                                });
+                                await _.editReply(await drawRankcard(_.guild!, user));
                             } catch (error) {
                                 await _.editReply(
                                     template.error("Sorry, something went wrong displaying your rank card. Please contact support if this issue persists."),
@@ -154,10 +152,7 @@ export default (app: Argentium) =>
                 ),
         );
 
-setInterval(
-    () => {
-        const now = Date.now();
-        lastMessage = Object.fromEntries(Object.entries(lastMessage).filter(([, v]) => now - v < 60000));
-    },
-    60 * 60 * 1000,
-);
+setInterval(() => {
+    const now = Date.now();
+    lastMessage = Object.fromEntries(Object.entries(lastMessage).filter(([, v]) => now - v < 60000));
+}, 60 * 60 * 1000);
